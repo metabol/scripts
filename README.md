153982950d62f4025577a0d9ef565684a5bd8c69
metabol <metabol@gmx.com>
2018-08-26 23:12:20 +0200

    Added useful links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Scripts
 
-This repository contains a collection of scripts I wrote. The scripts are in various languages.
+Useful links:
+- https://bencane.com
+- https://guide.bash.academy
---
+++ exit-code/error.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#__doc script to test exit codes
+
+touch /root/test 2> /dev/null
+
+if [ $? -eq 0 ]
+then
+  echo "Successfully created file"
+  exit 0
+else
+  echo "Could not create file" >&2
+  exit 1
+fi
